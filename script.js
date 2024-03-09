107042ad477ee330403801975cc92ce65ea5e9c3
LakshyaSharma207 <lakshyahero20@gmail.com>
2024-03-09 18:44:59 +0530

    media query and dark mode fix !important

--- script.js
+++ script.js
@@ -6,42 +6,37 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (prefersDarkMode) {
       document.body.classList.add("dark-mode");
-      darkModeToggle.src = "./toggle-on.png";
+      darkModeToggle.src = "./images/toggle-on.png";
       light.style.display = "none";
       dark.style.display = "inline-block";
-      document.getElementById("schedule").classList.add("dark-mode");
-      document.getElementById("team").classList.add("dark-mode");
+      document.body.classList.add("dark-mode");
+      // document.getElementById("team").classList.add("dark-mode");
     }
-
-    darkModeToggle.addEventListener("click", toggleDarkMode);
 
     function toggleDarkMode() {
       var body = document.body;
       body.classList.toggle("dark-mode");
 
       if (body.classList.contains("dark-mode")) {
-        darkModeToggle.src = "./toggle-on.png";
+        darkModeToggle.src = "./images/toggle-on.png";
         light.style.display = "none";
         dark.style.display = "inline-block";
         body.classList.add("dark-mode");
         document.getElementById("collapseOne").classList.add("dark-mode");
         document.getElementById("collapseTwo").classList.add("dark-mode");
         document.getElementById("collapseThree").classList.add("dark-mode");
-        document.getElementById("schedule").classList.add("dark-mode");
-        document.getElementById("team").classList.add("dark-mode");
+        document.body.classList.add("dark-mode");
       } else {
-        darkModeToggle.src = "./toggle-off.png";
+        darkModeToggle.src = "./images/toggle-off.png";
         dark.style.display = "none";
         light.style.display = "inline-block";
         body.classList.remove("dark-mode");
         document.getElementById("collapseOne").classList.remove("dark-mode");
         document.getElementById("collapseTwo").classList.remove("dark-mode");
         document.getElementById("collapseThree").classList.remove("dark-mode");
-        document.getElementById("schedule").classList.remove("dark-mode");
-        document.getElementById("team").classList.remove("dark-mode");
+        document.body.classList.remove("dark-mode"); 
       }
     }
 
+    darkModeToggle.addEventListener("click", toggleDarkMode);
   });
-
-
